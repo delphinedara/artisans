3f024bf8a6913443061609dcdacc88d7e4634aa5
Michael Ross Curnes <michaelrosscurnes@gmail.com>
2017-11-18 16:35:09 -0600

    adding passport

---
+++ model/user.js
@@ -0,0 +1,3 @@
+// this file will contain the model that describes the various parametes given to businesses/users
+
+// In a previous version this was a sequelize model for creating and storing user logins
