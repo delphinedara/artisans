bbcb944495774b43e035c65eaca28774c4a0b9b4
NancyDLL <nano14@mac.com>
2017-11-19 10:52:56 -0600

    cleaned up wireframe with nested grid layout

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,10 @@ const gulp = require('gulp');
 const autoprefixer = require('gulp-autoprefixer');
  
 gulp.task('default', () =>
-    gulp.src('assets/css/style.css')
+    gulp.src('client/public/assets/css/style.css')
         .pipe(autoprefixer({
             browsers: ['last 2 versions'],
             cascade: false
         }))
-        .pipe(gulp.dest('assets/css/'))
+        .pipe(gulp.dest('client/public/assets/css/'))
 );
